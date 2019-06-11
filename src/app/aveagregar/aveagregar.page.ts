3cd60822d53f76ca57bf5c082c0aa994f997b33e
Diego Quiroga <df.quiroga10@uniandes.edu.co>
2019-06-10 22:43:16 -0500

    Update aveagregar.page.ts

--- src/app/aveagregar/aveagregar.page.ts
+++ src/app/aveagregar/aveagregar.page.ts
@@ -18,7 +18,7 @@ constructor(
   	
     this.httpClient.post(`http://dev.contanimacion.com/birds/public/addBird/`,
     {
-      	idUser: localStorage.setItem('idUser', id),
+      	idUser: localStorage.getItem('idUser'),
 		bird_name: form.bird_name,
 		bird_description: form.bird_description,
 		place: form.place,
